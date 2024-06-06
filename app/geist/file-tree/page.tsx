17e06b80d3d03f1a28e79e082016bb4a91edf4d1
Anthony Young <404328+ayoungh@users.noreply.github.com>
2024-06-06 22:05:14 +0100

    add geist and update next version

---
+++ app/geist/file-tree/page.tsx
@@ -0,0 +1,83 @@
+"use client";
+
+import Image from "next/image";
+import { Tree } from "@geist-ui/core";
+
+export default function geist() {
+  return (
+    <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
+      <main className="flex flex-col gap-8 row-start-2 items-center sm:items-start">
+        <div className="flex items-center">
+          <h1 className="text-lg font-semibold md:text-2xl">File Tree</h1>
+        </div>
+        <div
+          className="flex flex-1 items-center justify-center rounded-lg border border-dashed shadow-sm"
+          x-chunk="dashboard-02-chunk-1"
+        >
+          {/* pnpm add @geist-ui/core */}
+
+          {/* <Tree>
+            <Tree.File name="package.json" />
+            <Tree.Folder name="components">
+              <Tree.File name="layout.js" />
+              <Tree.Folder name="footer">
+                <Tree.File name="footer.js" />
+                <Tree.File name="footer-text.js" />
+                <Tree.File name="footer-license.js" />
+              </Tree.Folder>
+              <Tree.File name="header.js" />
+            </Tree.Folder>
+            <Tree.File name="readme.md" />
+          </Tree> */}
+        </div>
+      </main>
+      <footer className="row-start-3 flex gap-6 flex-wrap items-center justify-center">
+        <a
+          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
+          href="https://geist-ui.dev/en-us/guide/introduction"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          <Image
+            aria-hidden
+            src="/file-text.svg"
+            alt="File icon"
+            width={16}
+            height={16}
+          />
+          Learn
+        </a>
+        <a
+          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
+          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          <Image
+            aria-hidden
+            src="/window.svg"
+            alt="Window icon"
+            width={16}
+            height={16}
+          />
+          Examples
+        </a>
+        <a
+          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
+          href="https://nextjs.org?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          <Image
+            aria-hidden
+            src="/globe.svg"
+            alt="Globe icon"
+            width={16}
+            height={16}
+          />
+          Go to nextjs.org →
+        </a>
+      </footer>
+    </div>
+  );
+}
